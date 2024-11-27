cde33c502835ad2de7917a8b48aa4f9a5de5613d
KillerSanss <blobisk@gmail.com>
2024-11-27 12:38:49 +0200

    Комит 1

--- Uchebka/Program.cs
+++ Uchebka/Program.cs
@@ -12,9 +12,6 @@ namespace Uchebka
 {
     internal static class Program
     {
-        /// <summary>
-        ///  The main entry point for the application.
-        /// </summary>
         [STAThread]
         static void Main()
         {
